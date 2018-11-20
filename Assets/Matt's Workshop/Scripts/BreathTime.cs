1d48ab768edb1e9e2fb86cbcadadb1cb9b22194c
Matt Lafontaine <mlafontaine18@gmail.com>
2018-11-19 18:06:19 -0800

    Merge branch 'DevBranch' into MattBranch
    - Added BreathTimer script - Tracks start and end time of experience and stage iteration
    - Began testing of script
    
    # Conflicts:
    #	Assets/RainStickStudio.meta
    #	Assets/RainStickStudio/Scenes.meta
    #	Assets/_Builds.meta
    #	Library/CurrentLayout.dwlt
    #	Library/LastSceneManagerSetup.txt
    #	Library/assetDatabase3
    #	Library/expandedItems
    #	Library/metadata/30/30abebfd9bf1c49d8a2d26e61e66bc15
    #	Library/metadata/43/43f10c83fd52885448478aaf5f929315
    #	Library/metadata/43/43f10c83fd52885448478aaf5f929315.info
    #	Library/metadata/59/59a911f90c5c78548bd0955487e88409
    #	Library/metadata/dd/dd15844c6a98bd744bc52048cb71107c
    #	Library/metadata/dd/dd15844c6a98bd744bc52048cb71107c.info
    #	Library/metadata/e1/e1c91a8eec9d3224dbe6abe711f15e0d.info
    #	Library/shadercompiler-UnityShaderCompiler.exe0.log

---
+++ Assets/Matt's Workshop/Scripts/BreathTime.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathTime : MonoBehaviour {
+
+
+    enum Stage
+    {
+        ONE = 0,
+        TWO = 1, 
+        THREE = 2
+    }
+
+    [Tooltip("Breathing stage timing.\n(x = Inhale, y = Pause, z = Exhale)")]
+    public List<Vector3> breathTimes;
+
+    [Tooltip("How many full breath cycles are made per stage.\n(x = Stage 1, y = Stage 2, z = Stage 3)")]
+    public Vector3 stageRepetitions;
+
+    [Tooltip("SET BY ENGINE\nTotal time each stage is active for.\n(x = Stage 1, y = Stage 2, z = Stage 3)")]
+    [SerializeField]
+    private Vector3 stageTimes;
+
+    private float totalExperienceTime;
+    private float startTime, endTime;
+
+	// Use this for initialization
+	void Start () {
+        stageTimes = GetStageTimeVector();
+        totalExperienceTime = GetTotalTime();
+
+        startTime = Time.time;
+        endTime = startTime + totalExperienceTime;
+
+        Debug.Log("Start Time " + startTime);
+        Debug.Log("End Time " + endTime);
+        Debug.Log("Stage Times: " + stageTimes);
+        Debug.Log("Total Experience Time: " + totalExperienceTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // GetTotalTime - returns total time experience is expected to take
+    private float GetTotalTime()
+    {
+        return stageTimes.x + stageTimes.y + stageTimes.z;
+    }
+    // GetStageTimeVector - places stage times in vector following Stage enum
+    private Vector3 GetStageTimeVector()
+    {
+        return new Vector3(GetStageTime((int)Stage.ONE), GetStageTime((int)Stage.TWO), GetStageTime((int)Stage.THREE));
+    }
+    // GetStageTime - returns span of time taken per stage
+    private float GetStageTime(int stage)
+    {
+        return 
+            (breathTimes[(int)stage].x + 
+             breathTimes[(int)stage].y + 
+             breathTimes[(int)stage].z) 
+           * stageRepetitions[(int)stage];
+    }
+}
