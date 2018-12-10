f8c5b813d0801a6b4e2fe47a98740e83aeebf38f
DESKTOP-LD91P4T\Lydia <Pixelpumpkiboo@gmail.com>
2018-12-09 19:30:04 -0800

    The script is done

---
+++ Assets/Time/Paticle_Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paticle_Timer : MonoBehaviour {
+
+	public float timer;
+	public float timeToStartP;
+
+	public GameObject[] allParticles;
+
+
+
+	void Update () 
+	{
+		timer += Time.deltaTime;
+
+		if (timer > timeToStartP) 
+		{
+			print ("OK");
+			foreach (GameObject _gameObject in allParticles) 
+			{
+				_gameObject.SetActive (true);
+				print ("OK");
+			}
+		}
+	}
+}
