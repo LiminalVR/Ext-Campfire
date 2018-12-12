69e22ea400c20954062b19334fb40e7147ca54b5
Connorreich <Connorreich27@gmail.com>
2018-12-12 11:41:40 -0800

    Merge branch 'DanielBranch' into ConnorBranch
    # Conflicts:
    #	RainStick.csproj

---
+++ Assets/RainStickStudio/Sound/SoundPrefab.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPrefab : MonoBehaviour {
+
+    public float minTime;
+    public float maxTime;
+
+    public float timeUntilPlayAgain;
+
+    public List<AudioClip> audioTracks = new List<AudioClip>();
+
+    public int CurrentTrackResult;
+
+    public AudioClip CurrentTrack;
+    public AudioSource audioOutput;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioOutput = GetComponent<AudioSource>();
+
+        timeUntilPlayAgain = Random.Range(minTime, maxTime);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeUntilPlayAgain -= Time.deltaTime;
+        if (timeUntilPlayAgain < 0)
+        {
+            CurrentTrackResult = Random.Range(0, audioTracks.Count);
+
+            CurrentTrack = audioTracks[CurrentTrackResult];
+            audioOutput.clip = CurrentTrack;
+            audioOutput.Play();
+            timeUntilPlayAgain = Random.Range(minTime, maxTime);
+        }
+	}
+}
