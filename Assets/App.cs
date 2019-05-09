e97fd6e7b0a3d194869c0f86bf32d7cb07c2d19c
StephenSchulze <stephen.s@liminalvr.com>
2019-05-09 14:13:22 +1000

    Moon rise + further polish and optimisation

---
+++ Assets/App.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class App : MonoBehaviour
+{
+    [Header("App Settings (Seconds)")]
+    public float FadeStartTime = 30;
+    public float TimeElapsed = 0;
+
+    public UnityEvent OnFadeBegin;
+
+    private void Start()
+    {
+        StartCoroutine(Loop());
+        StartCoroutine(DisplayTimeLapsed());
+    }
+
+    private IEnumerator Loop()
+    {
+        yield return new WaitForSeconds(FadeStartTime);
+        OnFadeBegin?.Invoke();
+    }
+
+    private IEnumerator DisplayTimeLapsed()
+    {
+        var startTime = Time.time;
+        while (true)
+        {
+            TimeElapsed = Time.time - startTime;
+            yield return null;
+        }
+    }
+}
